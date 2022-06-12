6623520a47f7dbdbcdc46d03aecd156f8a16c51e
Rahele <raheleh.tabasi@gmail.com>
2022-06-12 21:51:08 +0430

    add bussiness layer and service for customer

---
+++ WebApi.Bussiness/Services/Interfaces/ICustomerService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApi.Data.Entities;
+
+namespace WebApi.Bussiness.Services.Interfaces
+{
+    public interface ICustomerService
+    {
+        List<Customer> GetAllCustomer();
+        Customer GetCustomerById(int customerId);
+        void InsertCustomer(Customer customer);
+        void UpdateCustomer(Customer customer);
+        Customer DeleteCustomer(int customerId);
+
+        bool ExistCustomer(int customerId);
+    }
+}
